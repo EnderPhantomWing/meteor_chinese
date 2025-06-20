dce9886ca7f006f96dbcf45612cea7d5d2d05a97
Nippaku_Zanmu <120476813+Nippaku-Zanmu@users.noreply.github.com>
2025-06-20 14:17:06 +0800

    重构翻译逻辑
    重构翻译逻辑
    dump去重
    支持1.21.6

--- src/main/java/com/nippaku_zanmu/trans_addon/modules/Translation.java
+++ src/main/java/com/nippaku_zanmu/trans_addon/modules/Translation.java
@@ -3,10 +3,13 @@ package com.nippaku_zanmu.trans_addon.modules;
 
 import com.nippaku_zanmu.trans_addon.MeteorTranslation;
 import com.nippaku_zanmu.trans_addon.settings.StringSelectSetting;
+import com.nippaku_zanmu.trans_addon.util.JsonDump;
 import com.nippaku_zanmu.trans_addon.util.TransUtil;
 import com.nippaku_zanmu.trans_addon.mixin.ModuleAccessor;
 import com.nippaku_zanmu.trans_addon.mixin.SettingAccessor;
 import com.nippaku_zanmu.trans_addon.mixin.SettingGroupAccessor;
+import com.nippaku_zanmu.trans_addon.util.trans_engine.AbstractTransEngine;
+import com.nippaku_zanmu.trans_addon.util.trans_engine.EngineManager;
 import meteordevelopment.meteorclient.gui.GuiTheme;
 import meteordevelopment.meteorclient.gui.widgets.WWidget;
 import meteordevelopment.meteorclient.gui.widgets.containers.WHorizontalList;
@@ -17,30 +20,48 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.utils.Utils;
 import meteordevelopment.meteorclient.utils.player.ChatUtils;
-import net.minecraft.text.Text;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Set;
 
 public class Translation extends Module {
     private final SettingGroup sgGeneral = this.settings.getDefaultGroup();
+
     public final Setting<Boolean> bSetAutoTranslation = sgGeneral.add(new BoolSetting.Builder()
         .name("auto-translation")
         .description("")
         .defaultValue(false)
         .build());
-    public final Setting<String> sSetDumpPath = sgGeneral.add(new StringSetting.Builder()
-        .name("DumpPath")
+
+    public final Setting<Set<String>> translationModules = sgGeneral.add(new StringSelectSetting.Builder()
+        .validValues(TransUtil.getAddonNames())
+        .defaultValue(TransUtil.getAddonNames())
+        .name("translation-modules")
+        .build());
+
+    private final SettingGroup sgDev = this.settings.createGroup("Dev", false);
+
+    public final Setting<String> strSetTransEngine = sgDev.add(new StringSetting.Builder()
+        .defaultValue("NEW")
+        .name("translation-engine")
+        .build());
+
+
+    public final Setting<String> sSetDumpPath = sgDev.add(new StringSetting.Builder()
+        .name("dump-path")
         .defaultValue("D:\\hack\\Misc\\meteor-translation-addon\\test\\en_us.json")
         .build());
-    public final Setting<Boolean> bSetDumpFile = sgGeneral.add(new BoolSetting.Builder()
-        .name("dump en_usJson")
+
+    public final Setting<Boolean> bSetDumpText = sgDev.add(new BoolSetting.Builder()
+        .name("dump-text")
         .defaultValue(false)
+        .build()
+    );
+    public final Setting<String> strSetDumpTextEngine = sgDev.add(new StringSetting.Builder()
+        .defaultValue("OLD")
+        .visible(bSetDumpText::get)
+        .name("dump-text-engine")
         .build());
-    public final Setting<Set<String>> translationModules = sgGeneral.add(new StringSelectSetting.Builder().validValues(TransUtil.getAddonsName())
-        .defaultValue(TransUtil.getAddonsName()).name("translation-modules").build());
-    public static BufferedWriter dumpBW;
+
 
     public Translation() {
         super(MeteorTranslation.CATEGORY, "meteor-trans", "An example module that highlights the center of the world.");
@@ -59,72 +80,61 @@ public class Translation extends Module {
         ChatUtils.warning("如果你购买了此插件 则说明你被骗了");
     }
 
-    //    @Override
-//    public void onActivate() {
-//        tran();
-//    }
+
     @Override
     public WWidget getWidget(GuiTheme theme) {
         WVerticalList list = theme.verticalList();
 
-        WHorizontalList b = list.add(theme.horizontalList()).expandX().widget();
+        WHorizontalList l1 = list.add(theme.horizontalList()).expandX().widget();
 
-        WButton start = b.add(theme.button("Translate")).expandX().widget();
+        WButton start = l1.add(theme.button("Translate")).expandX().widget();
         start.action = () -> {
             if (this.isActive()) {
-                if (bSetDumpFile.get()) {
-                    try {
-                        File path = new File(sSetDumpPath.get());
-                        if (!path.exists() & !(path.createNewFile())) {
-                            ChatUtils.sendMsg(Text.of("DumpError Can't Create Dump File"));
-                            return;
-                        }
-                        dumpBW = new BufferedWriter(new OutputStreamWriter(
-                            new FileOutputStream(path, false), StandardCharsets.UTF_8));
-                    } catch (IOException e) {
-                        ChatUtils.error(e.getMessage());
-                        return;
-                    }
-                }
                 isTranslation = true;
                 tran();
-
-                try {
-                    if (dumpBW != null)
-                        dumpBW.close();
-                } catch (IOException e) {
-
-                }
-
             } else {
                 ChatUtils.warning("你首先要开启此模块");
             }
+        };
+
+        if (!sgDev.sectionExpanded)
+            return list;
+        WHorizontalList l2 = list.add(theme.horizontalList()).expandX().widget();
+        WButton dump = l2.add(theme.button("Dump")).expandX().widget();
+        dump.action = () -> {
+            JsonDump.getINSTANCE().write(EngineManager.getInstance().getEngine(strSetTransEngine.get()), EngineManager.getInstance().getEngine(strSetDumpTextEngine.get()));
         };
         return list;
     }
 
 
     public void tran() {
+        tran(EngineManager.getInstance().getEngine(strSetTransEngine.get()));
+    }
+
+
+    private void tran(AbstractTransEngine engine) {
         for (Module module : Modules.get().getAll()) {
             String addonName = TransUtil.getAddonName(module);
             if (!translationModules.get().contains(addonName)) continue;
             //插件过滤
 
-            String tranName = TransUtil.transModuleName(module);
+            String tranName = engine.transModuleName(module);
             // 经过翻译的名称
             ((ModuleAccessor) module).setTitle(Utils.nameToTitle(tranName));
             //把标题设为翻译之后的名称
 
-            String tranDescry = TransUtil.transModuleDescription(module);
+            String tranDescry = engine.transModuleDescription(module);
             ((ModuleAccessor) module).setDescription(Utils.nameToTitle(tranDescry));
             //翻译简介
 
             for (SettingGroup group : module.settings.groups) {
                 for (Setting<?> setting : ((SettingGroupAccessor) group).getSettings()) {
-                    String tranSettName = TransUtil.transSettingName(module, group, setting);
+
+                    String tranSettName = engine.transSettingName(module, group, setting);
                     ((SettingAccessor) setting).setTitle(Utils.nameToTitle(tranSettName));
 
-                    String tranSettDesc = TransUtil.transSettingDes(module, group, setting);
+                    String tranSettDesc = engine.transSettingDes(module, group, setting);
                     ((SettingAccessor) setting).setDescription(Utils.nameToTitle(tranSettDesc));
                 }
             }
--- src/main/java/com/nippaku_zanmu/trans_addon/util/TransUtil.java
+++ src/main/java/com/nippaku_zanmu/trans_addon/util/TransUtil.java
@@ -1,20 +1,10 @@
 package com.nippaku_zanmu.trans_addon.util;
 
-import com.nippaku_zanmu.trans_addon.MeteorTranslation;
-import com.nippaku_zanmu.trans_addon.modules.Translation;
 import meteordevelopment.meteorclient.addons.AddonManager;
-import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.settings.Setting;
-import meteordevelopment.meteorclient.settings.SettingGroup;
-import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.utils.player.ChatUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -24,8 +14,16 @@ public class TransUtil {
     private static String trans(String s) {
         return Text.translatable(s).getString();
     }
+    public static String trans(String key,String alternative){
+        String trans = trans(key);
+        //调用mc函数翻译
+        if (trans.equals(key)) {
+            return alternative;
+        }//如果没有翻译 即翻译后的还是原本的key 则返回原名称
+        return trans;
+    }
 
-    public static Set<String> getAddonsName() {
+    public static Set<String> getAddonNames() {
         return AddonManager.ADDONS.stream().map(addon -> addon.name).map(TransUtil::baseFormat).collect(Collectors.toSet());
     }
 
@@ -33,109 +31,17 @@ public class TransUtil {
         return TransUtil.baseFormat(m.addon == null ? "unknow_addon" : m.addon.name);
     }
 
-    public static String transModuleName(Module m) {
-        Category category = m.category;
-        String moduleName = m.name;
-        String key = "meteor." + getAddonName(m) + "." + baseFormat(category.name) + "." + baseFormat(moduleName);
-        //转换成规范的key
-        //meteor.模块类型.模块名
-
-        dumpEn_USJson(key, moduleName);
-        //开发者使用 导出英文语言文件
-
-        String trans = trans(key);
-        //调用mc函数翻译
-
-        if (trans.equals(key)) {
-            return moduleName;
-        }//如果没有翻译 即翻译后的还是原本的key 则返回原模块名称
-        return trans;
-    }
-
-    public static String transModuleDescription(Module m) {
-        Category category = m.category;
-        String moduleName = m.name;
-        String moduleDes = m.description;
-        String key = "meteor." + getAddonName(m) + "." + baseFormat(category.name) + "." + baseFormat(moduleName) + ".description";
-        //转换成规范的key
-        dumpEn_USJson(key, moduleDes);
-
-        String trans = trans(key);
-        //调用mc函数翻译
-
-        if (trans.equals(key)) {
-            return moduleDes;
-        }//如果没有翻译 即翻译后的还是原本的key 则返回原名称
-        return trans;
-    }
-
-    public static String transSettingName(Module module, SettingGroup group, Setting s) {
-        String key = "meteor." +
-            getAddonName(module) + "." +
-            baseFormat(module.category.name) + "." +
-            baseFormat(module.name) + "." +
-            "setting." +
-            group.name + "." +
-            s.name;
-
-        dumpEn_USJson(key, s.name);
-        //dump
-
-        String trans = trans(key);
-        //调用mc函数翻译
-
-        if (trans.equals(key)) {
-            return s.name;
-        }//如果没有翻译 即翻译后的还是原本的key 则返回原名称
-        return trans;
-    }
-
-    public static String transSettingDes(Module module, SettingGroup group, Setting s) {
-        String key = "meteor." +
-            getAddonName(module) + "." +
-            baseFormat(module.category.name) + "." +
-            baseFormat(module.name) + "." +
-            "setting." +
-            group.name + "." +
-            s.name + "." +
-            "description";
-
-        dumpEn_USJson(key, s.description);
-        //dump
-
-        String trans = trans(key);
-        //调用mc函数翻译
-
-        if (trans.equals(key)) {
-            return s.description;
-        }//如果没有翻译 即翻译后的还是原本的key 则返回原名称
-        return trans;
-    }
-
-
     public static String baseFormat(String s) {
         //把某些Addon作者的不规范模块命名还原
         s = s.toLowerCase();
         s = s.replace(" ", "_");
         s = s.replace("-", "_");
         s = s.replace(".", "_");
+        s = s.replace("\"", "_");
         return s;
     }
-
-    public static void dumpEn_USJson(String key, String en_usName) {
-        Translation translation = Modules.get().get(Translation.class);
-
-        if ((!translation.bSetDumpFile.get()) || translation.bSetAutoTranslation.get() || mc.world == null || mc.player == null)
-            return;
-        try {
-
-            en_usName = en_usName.replace("\"", "\\\"");
-            Translation.dumpBW.write("\"" + key + "\"" + ":" + "\"" + en_usName + "\"" + ",");
-            Translation.dumpBW.newLine();
-            Translation.dumpBW.flush();
-        } catch (IOException e) {
-            ChatUtils.error(e.getMessage());
-        }
+    public static String formatValue(String s){
+        s = s.replace("\"", "\\\"");
+        return s;
     }
-
 }
